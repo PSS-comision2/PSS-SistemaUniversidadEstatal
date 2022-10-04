0e220d3945f41dc16d5ff8bead814148e9440905
SeleneGonzalo <selenegonzalo89@gmail.com>
2022-10-04 01:12:46 -0300

    diseño mejorado y eliminación de rutas extras

--- resources/views/profesor/auth/login.blade.php
+++ resources/views/profesor/auth/login.blade.php
@@ -1,9 +1,12 @@
-<x-profesor-guest-layout>
+<x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
+        <div class="div" style="opacity: 50%; font-size: 40px">
+                Universidad Estatal
+            </div>
+            <div class="div" style="opacity: 50%; font-size: 18px; text-align: center;">
+                Profesor
+            </div>
         </x-slot>
 
         <!-- Session Status -->
@@ -17,40 +20,26 @@
 
             <!-- legajo -->
             <div>
-                <x-label for="legajo" :value="__('Email')" />
+                <x-label for="legajo" :value="__('Legajo')" />
 
                 <x-input id="legajo" class="block mt-1 w-full" type="integer" name="legajo" :value="old('legajo')" required autofocus />
             </div>
 
             <!-- Password -->
             <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+                <x-label for="password" :value="__('Contraseña')" />
 
                 <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
-
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
+                    type="password"
+                    name="password"
+                    required autocomplete="current-password" />
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('profesor.password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('profesor.password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
                 <x-button class="ml-3">
-                    {{ __('Log in') }}
+                    {{ __('Ingresar') }}
                 </x-button>
             </div>
         </form>
     </x-auth-card>
-</x-profesor-guest-layout>
+</x-guest-layout>
