13844b24b002b9f3b17b598b0db25cefe80430b9
Fermin <feeralvarez013@gmail.com>
2022-10-15 13:21:01 -0300

    Cierra sesión con un solo click

--- resources/views/administrador/layouts/navbar.blade.php
+++ resources/views/administrador/layouts/navbar.blade.php
@@ -48,7 +48,7 @@
         <ul class="menu menu-horizontal p-0">
             <li tabindex="0">
                 <a>
-                    {{ Auth::user()->nombre}}  {{Auth::user()->apellido }}
+                    {{ Auth::user()->nombre }} {{ Auth::user()->apellido }}
                     <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                         viewBox="0 0 24 24">
                         <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
@@ -57,8 +57,7 @@
                 <ul class="btn btn-outline">
                     <li>
                         <form id="logout-form" action="{{ route('administrador.logout') }}" method="POST">
-                            <a href="javascript:;" class=""
-                                onclick="document.getElementById('logout-form').submit();">
+                            <a onclick="document.getElementById('logout-form').submit();">
                                 <span class=" ">Cerrar sesión</span>
                             </a>
                             @csrf
