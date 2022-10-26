4d726335130719ada52d2450e3cdcc884256a806
Fermin <feeralvarez013@gmail.com>
2022-10-26 14:01:31 -0300

    Corrección mostrar carreras alumnos

--- resources/views/alumno/mostrarcarrerasinscriptas.blade.php
+++ resources/views/alumno/mostrarcarrerasinscriptas.blade.php
@@ -38,7 +38,7 @@
                                       </label>
                                     </div>
                                     <div class="2xl:w-2/3">
-                                      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="text" value="{{$carrerasinscripto[0]->carrera->numero_plan}}" id="carreranumeroplan" name="carreranumeroplan" disabled>
+                                      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="text" value="{{$carrerasinscripto->first()->carrera->numero_plan}}" id="carreranumeroplan" name="carreranumeroplan" disabled>
                                     </div>
                                   </div>
 
@@ -49,7 +49,7 @@
                                       </label>
                                     </div>
                                     <div class="2xl:w-2/3">
-                                      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="text" value="{{$carrerasinscripto[0]->carrera->departamento}}"id="carreradepartamento" name="carreradepartamento" disabled>
+                                      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="text" value="{{$carrerasinscripto->first()->carrera->departamento}}"id="carreradepartamento" name="carreradepartamento" disabled>
                                     </div>
                                   </div>
                             </div>
